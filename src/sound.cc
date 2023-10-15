733f544f61e7a87a68364f0638d4190150711a19
Hiroshi Tsubokawa <hiroshi@fujiyama-renderer.com>
2023-10-14 23:25:49 -0700

    Add SDL audio

--- src/sound.cc
+++ src/sound.cc
@@ -1,7 +1,17 @@
 #include <algorithm>
+#include <iostream>
 #include <vector>
+
+#define USE_OPENAL 0
+#define USE_CALLBACK 1
+#if USE_OPENAL
 #include <al.h>
 #include <alc.h>
+#else
+#include <SDL.h>
+#include <queue>
+#include <deque>
+#endif
 
 #include "sound.h"
 
@@ -9,14 +19,23 @@ namespace nes {
 
 constexpr int SAMPLING_RATE = 44100;
 constexpr int MAX_SAMPLE_COUNT = 8 * SAMPLING_RATE / 60;
+#if USE_OPENAL
 constexpr int BUFFER_COUNT = 32;
+#endif
 
+#if USE_OPENAL
 static ALCdevice *device = nullptr;
 static ALCcontext *context = nullptr;
 
 static ALuint source = 0;
 static ALuint buffer_list[BUFFER_COUNT] = {0};
 static ALuint *pbuffer = buffer_list;
+#else
+static SDL_AudioDeviceID audio_device;
+//static std::queue<float> sample_queue;
+static std::deque<float> sample_queue(1024 * 8, 0);
+void fill_audio(void *udata, Uint8 *stream, int len);
+#endif
 
 class SampleBuffer {
 public:
@@ -44,6 +63,7 @@ public:
 
     const int16_t *Data() const { return &data_[0]; }
     int Size() const { return count_ * sizeof(data_[0]); }
+    int Count() const { return count_; }
 
 private:
     std::vector<int16_t> data_;
@@ -54,18 +74,41 @@ static SampleBuffer sample_data;
 
 void InitSound()
 {
+#if USE_OPENAL
     device = alcOpenDevice(nullptr);
     context = alcCreateContext(device, nullptr);
     alcMakeContextCurrent(context);
 
     alGenBuffers(BUFFER_COUNT, buffer_list);
     alGenSources(1, &source);
+#else
+    //Initialize SDL
+    //if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
+    if(SDL_Init(SDL_INIT_AUDIO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        //return -1;
+        return;
+    }
+
+    // Audio
+    SDL_AudioSpec audio_spec;
+    SDL_zero(audio_spec);
+    audio_spec.freq = SAMPLING_RATE;
+    audio_spec.format = AUDIO_S16SYS;
+    audio_spec.channels = 1;
+    audio_spec.samples = 1024 / 1;
+    audio_spec.callback = nullptr;
+    if (USE_CALLBACK)
+        audio_spec.callback = fill_audio;
+    audio_device = SDL_OpenAudioDevice(nullptr, 0, &audio_spec, nullptr, 0);
+#endif
 
     sample_data.Resize(MAX_SAMPLE_COUNT);
 }
 
 void FinishSound()
 {
+#if USE_OPENAL
     alSourceStop(source);
 
     alDeleteBuffers(BUFFER_COUNT, buffer_list);
@@ -74,12 +117,16 @@ void FinishSound()
     alcMakeContextCurrent(nullptr);
     alcDestroyContext(context);
     alcCloseDevice(device);
+#else
+    SDL_CloseAudioDevice(audio_device);
+#endif
 
     sample_data.Clear();
 }
 
 static void unqueue_buffer()
 {
+#if USE_OPENAL
     for (;;) {
         ALuint unqueued_buff[8] = {0};
         ALint processed_count = 0;
@@ -90,34 +137,66 @@ static void unqueue_buffer()
         else
             break;
     }
+#else
+#endif
 }
 
 static void switch_buffer()
 {
+#if USE_OPENAL
     if (pbuffer == &buffer_list[BUFFER_COUNT - 1])
         pbuffer = buffer_list;
     else
         pbuffer++;
+#else
+#endif
 }
 
 static void queue_buffer(const SampleBuffer &buff)
 {
+#if USE_OPENAL
     // copy sample buff
     alBufferData(*pbuffer, AL_FORMAT_MONO16, buff.Data(), buff.Size(), SAMPLING_RATE);
 
     // queue buffer
     alSourceQueueBuffers(source, 1, pbuffer);
+#else
+    SDL_QueueAudio(audio_device, buff.Data(), buff.Size());
+#endif
 
     switch_buffer();
 }
 
 void PushSample(float sample)
 {
+#if USE_OPENAL
     sample_data.Push(INT16_MAX * sample);
+#else
+    if (USE_CALLBACK) {
+        SDL_LockAudioDevice(audio_device);
+        sample_queue.push_back(sample);
+        SDL_UnlockAudioDevice(audio_device);
+    }
+    else {
+        sample_data.Push(INT16_MAX * sample * .5);
+    }
+    //const int16_t data = INT16_MAX * sample * .5;
+    //SDL_QueueAudio(audio_device, &data, sizeof(data));
+    //SDL_LockAudioDevice(audio_device);
+    //SDL_UnlockAudioDevice(audio_device);
+#endif
 }
 
 void SendSamples()
 {
+    if (USE_CALLBACK && !USE_OPENAL)
+        return;
+
+#if USE_OPENAL
+    printf("OpenAL samples: %d\n", sample_data.Count());
+#else
+    printf("SDL2 samples: %d\n", sample_data.Count());
+#endif
     unqueue_buffer();
     queue_buffer(sample_data);
 
@@ -126,6 +205,7 @@ void SendSamples()
 
 void PlaySamples()
 {
+#if USE_OPENAL
     ALint queued_count = 0;
     ALint stat = 0;
 
@@ -138,11 +218,63 @@ void PlaySamples()
         if (0)
             printf("Sound Play: Queued Count: %d\n", queued_count);
     }
+#else
+    SDL_PauseAudioDevice(audio_device, 0);
+#endif
 }
 
 void PauseSamples()
 {
+#if USE_OPENAL
     alSourcePause(source);
+#else
+    SDL_PauseAudioDevice(audio_device, 1);
+#endif
 }
+
+int GetQueuedSamples()
+{
+#if USE_OPENAL
+    return 0;
+#else
+    const Uint32 count = SDL_GetQueuedAudioSize(audio_device) / sizeof(int16_t);
+    //printf("queued samples: %d\n", count);
+    return count;
+#endif
+}
+
+#if USE_OPENAL
+#else
+// The audio function callback takes the following parameters:
+// stream:  A pointer to the audio buffer to be filled
+// len:     The length (in bytes) of the audio buffer
+void fill_audio(void *udata, Uint8 *stream, int len)
+{
+    const int n = sample_queue.size();
+    const int N = len / 2;
+    int16_t *stream16 = reinterpret_cast<int16_t*>(stream);
+    static int16_t sample = 0;
+
+    if (n < N) {
+        for (int i = 0; i < N; i++) {
+            stream16[i] = sample;
+        }
+        SDL_PauseAudioDevice(audio_device, 0);
+        return;
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (!sample_queue.empty()) {
+            sample = sample_queue.front() * INT16_MAX * .5;
+            sample_queue.pop_front();
+        }
+        else {
+            //printf("i: %d, size: %d, N: %d\n", i, n, N);
+        }
+
+        stream16[i] = sample;
+    }
+}
+#endif
 
 } // namespace
